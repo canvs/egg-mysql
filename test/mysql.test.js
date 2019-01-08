e1015eb89cca80c94fa8498249d07bd22fe372d4
东隅 <476685852@qq.com>
2019-01-08 13:55:25 +0800

    private npm version

--- test/mysql.test.js
+++ test/mysql.test.js
@@ -83,6 +83,15 @@ describe('test/mysql.test.js', () => {
     const row = yield app.mysql.get('npm_auth', { user_id: user.user_id });
     assert(row.id === user.id);
   });
+  
+  it('should query one desc is NULL success', function* () {
+    const user = yield app.mysql.queryOne('select * from npm_auth where `desc` is NULL');
+    assert(user);
+    assert(typeof user.user_id === 'string' && user.user_id);
+
+    const row = yield app.mysql.get('npm_auth', { desc: null });
+    assert(row.id === user.id);
+  });
 
   it('should query one not exists return null', function* () {
     let user = yield app.mysql.queryOne('select * from npm_auth where id = -1');
